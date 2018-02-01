0ca994c8e907a639346385d85dbdf7a8113737f8
Aosanders <sanderscross@gmail.com>
2018-02-01 13:47:54 -0800

    Nav Bar scroll functionality implemented

--- js/main.js
+++ js/main.js
@@ -17,4 +17,42 @@ $(document).ready(function(){
         $("html, body").animate({scrollTop: 0}, "slow");
    });
 
+   $(".header a").on("click", function(evt){
+      evt.preventDefault();
+
+      var $navId = $(this).attr("id");
+      var $section;
+
+      switch($navId){
+        case "homeNav":
+          $section = "#section-1";
+          goToSection($section);
+          break;
+
+        case "aboutNav":
+        $section = "#section-2";
+        goToSection($section);
+          break;
+
+        case "serviceNav":
+        $section = "#section-3";
+        goToSection($section);
+          break;
+
+        case "contactNav":
+        $section = "#section-4";
+        goToSection($section);
+          break;
+
+        default:
+          alert("nothing here");
+      }
+   });
+
+   function goToSection(section){
+      var sectionLoc = $(section).offset().top;
+      var headerHeight = $(".header").css("height").slice(0,-2);
+      $("html, body").animate({scrollTop: (sectionLoc - headerHeight)}, 1200);
+   }
+
 });
